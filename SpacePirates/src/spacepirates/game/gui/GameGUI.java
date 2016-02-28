914e9df28bd1adaecd464fd1c01f5dee149c06f2
eyneill777 <eyneill777@gmail.com>
2016-02-28 12:16:48 -0500

    HUD stuff

---
+++ SpacePirates/src/spacepirates/game/gui/GameGUI.java
@@ -0,0 +1,21 @@
+package spacepirates.game.gui;
+
+import java.util.ArrayList;
+
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+public class GameGUI 
+{
+	ArrayList<GameHudObject> hudObjects = new ArrayList<GameHudObject>();
+	
+	public GameGUI()
+	{
+		
+	}
+	
+	public void render(SpriteBatch batch)
+	{
+		
+	}
+}
